8128662692f067a70b46a5fee9e2654a7028121b
hasan-naim <hasannaim6246@gmail.com>
2021-09-16 22:55:37 +0600

    done with the carousel

---
+++ js/main.js
@@ -0,0 +1,28 @@
+
+
+// code for owl carousel
+$(document).ready(function() {
+    $('.owl-carousel').owlCarousel({
+        margin: 40,
+        loop: true,
+        dots: true,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        autoplayHoverPause: true,
+        responsive : {
+            // breakpoint from 0 up
+            0 : {
+                items: 1
+            },
+            // breakpoint from 560 up
+            560 : {
+                items: 2
+            },
+            // breakpoint from 850 up
+            850 : {
+                items: 3
+            }
+        }
+
+    });
+});
