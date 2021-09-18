30f60e3a62dccf71d42038c2a3042c33ca3ac2c8
hasan-naim <hasannaim6246@gmail.com>
2021-09-18 12:35:05 +0600

    treanding products section is completed

--- js/main.js
+++ js/main.js
@@ -26,3 +26,31 @@ $(document).ready(function() {
 
     });
 });
+
+///
+
+// $(document).ready(function() {
+//     $('.owl-carousel').owlCarousel({
+//         margin: 40,
+//         loop: true,
+//         dots: true,
+//         autoplay: true,
+//         autoplayTimeout: 2000,
+//         autoplayHoverPause: true,
+//         responsive : {
+//             // breakpoint from 0 up
+//             0 : {
+//                 items: 1
+//             },
+//             // breakpoint from 560 up
+//             560 : {
+//                 items: 2
+//             },
+//             // breakpoint from 850 up
+//             850 : {
+//                 items: 3
+//             }
+//         }
+
+//     });
+// });
