9928dc588ba0f93564138c0218c98e04f6d29a6b
hasan-naim <hasannaim6246@gmail.com>
2021-09-21 19:51:58 +0600

    review section done

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 
 // code for owl carousel
 $(document).ready(function() {
-    $('.owl-carousel').owlCarousel({
+    $('.owl-heroCarousel').owlCarousel({
         margin: 40,
         loop: true,
         dots: true,
@@ -24,6 +24,34 @@ $(document).ready(function() {
             }
         }
 
+    });
+
+    // owl carousel for review section
+    $(".owl-review").owlCarousel({
+        margin: 20,
+        loop: true,
+        dots: true,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        autoplayHoverPause: true,
+        responsive : {
+            // breakpoint from 0 up
+            0 : {
+                items: 1
+            },
+            // breakpoint from 560 up
+            520 : {
+                items: 2
+            },
+            // breakpoint from 850 up
+            860 : {
+                items: 3
+            },
+            // breakpoint from 1080 up
+            1080 : {
+                items: 4
+            }
+        }
     });
 });
 
